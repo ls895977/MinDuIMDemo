41b971885ae188dc128faddf0acee78d9acbc871
xiapeng <1qaz2wsx>
2020-12-07 15:13:33 +0800

    maven

---
+++ IM-kit/src/main/java/com/css/im_kit/db/Test.kt
@@ -0,0 +1,11 @@
+package com.css.im_kit.db
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * 土司扩展
+ */
+fun String.toast11(context: Context) {
+    Toast.makeText(context, this, Toast.LENGTH_SHORT)
+}
