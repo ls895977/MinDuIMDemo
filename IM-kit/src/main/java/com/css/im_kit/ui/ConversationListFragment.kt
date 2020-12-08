f1cb0527c7530d0e061aba21b26f982293c95ae2
kairy <951809209@qq.com>
2020-12-08 18:06:24 +0800

    基本模型

--- IM-kit/src/main/java/com/css/im_kit/ui/ConversationListFragment.kt
+++ IM-kit/src/main/java/com/css/im_kit/ui/ConversationListFragment.kt
@@ -4,15 +4,19 @@ import com.css.im_kit.R
 import com.css.im_kit.databinding.FragmentConversationListBinding
 import com.css.im_kit.db.bean.User_Info
 import com.css.im_kit.ui.base.BaseFragment
+import com.scwang.smartrefresh.layout.api.RefreshLayout
 
 class ConversationListFragment : BaseFragment<FragmentConversationListBinding?>() {
 
-
     override fun layoutResource(): Int = R.layout.fragment_conversation_list
-
-    override fun initView() {
-    }
-
+    override fun initView() {}
     override fun initData() {}
-    override fun initListeners() {}
+    override fun initListeners() {
+        binding!!.refreshView.setOnLoadMoreListener { refreshLayout: RefreshLayout ->
+            refreshLayout.finishLoadMore()
+        }
+        binding!!.refreshView.setOnRefreshListener { refreshLayout: RefreshLayout ->
+            refreshLayout.finishRefresh()
+        }
+    }
 }
