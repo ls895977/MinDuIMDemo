2ffe44465bd5fa354dd5cb69b807aa4a91b9073d
xiapeng <1qaz2wsx>
2020-12-10 09:54:36 +0800

    解决报错

--- IM-kit/src/main/java/com/css/im_kit/model/userinfo/SGUserInfo.kt
+++ IM-kit/src/main/java/com/css/im_kit/model/userinfo/SGUserInfo.kt
@@ -4,4 +4,13 @@ class SGUserInfo {
     var userId: String? = null
     var userName: String? = null
     var avatar: String? = null
+
+    constructor(userId: String?, userName: String?, avatar: String?) {
+        this.userId = userId
+        this.userName = userName
+        this.avatar = avatar
+    }
+
+    constructor()
+
 }
