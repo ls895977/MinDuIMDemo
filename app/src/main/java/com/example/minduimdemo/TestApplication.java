e15633fc7ebe8448cbf15152d69e467c78a8f7b3
kairy <951809209@qq.com>
2020-12-30 18:18:48 +0800

    图片展示问题

--- app/src/main/java/com/example/minduimdemo/TestApplication.java
+++ app/src/main/java/com/example/minduimdemo/TestApplication.java
@@ -23,6 +23,7 @@ public class TestApplication extends Application {
         IMManager.INSTANCE.build(this, "1607505722", "uN29TNVRilVmwqHw7aIMQfDZyw2ltVZO");
         IMManager.INSTANCE.setIMURL("http://devchatapi.supersg.cn", "/chat/listC");
         IMManager.INSTANCE.setQiuNiuTokenUrl("http://testcustomerapi.supersg.cn/api/customer_app/qiniu/getConfig");
+        IMManager.INSTANCE.setBusiness(true);
     }
 
     //static 代码段可以防止内存泄露
